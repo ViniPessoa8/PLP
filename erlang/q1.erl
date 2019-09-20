f293df11dd068dd7d484133740815488c20e9220
Matheus Soares <m.soares.ms@outlook.com>
2019-09-19 23:10:42 -0400

    q1 - soma

---
+++ erlang/q1.erl
@@ -0,0 +1,10 @@
+-module(q1).
+-export([start/0]).
+
+soma(A, B) ->
+io:fwrite("~w~n", [A+B]).
+
+start() -> 
+    {ok, A} = io:read("Enter A: "),
+    {ok, B} = io:read("Enter B: "),
+    soma(A, B).
