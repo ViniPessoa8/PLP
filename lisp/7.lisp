2cca3c61e229a833dd44a82d2aec7d6703cd4b26
Ericaasca <54380638+Ericaasca@users.noreply.github.com>
2019-09-24 16:51:58 -0400

    Questao 7 e 9 corrigidas

--- lisp/7.lisp
+++ lisp/7.lisp
@@ -1,3 +1,14 @@
+(defun primo(n)
+    (if (<= n 1) (return-from primo nil))
+    (loop for i from 2 to n
+        do (if (eq (mod n i) 0)
+            (return-from primo nil)
+            (return-from primo t)
+        )
+    )
+)
+
 (defun main()
-    (princ (oddp (read)))
+    (princ (primo (read)))
+    (fresh-line)
 )
